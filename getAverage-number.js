f04b726e363a0887cf348b306492b2f47234dc28
Rubel <rubelhosen1310@gmail.com>
2025-02-07 14:58:32 +0600

    added average number js

---
+++ getAverage-number.js
@@ -0,0 +1,11 @@
+var Math = 40.25;
+var Biology = 65;
+var Chemistry =80;
+var Physics = 45;
+var Bangla = 99.50;
+var English = 95.65
+
+var totalMarks = Math + Biology + Chemistry + Physics + Bangla;
+
+var averageMarks = totalMarks / 6;
+console.log(averageMarks)
