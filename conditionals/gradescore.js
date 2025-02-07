bcdc2df81d75929019a12fd1ad0b2e3273c6c0f4
Rubel <rubelhosen1310@gmail.com>
2025-02-07 16:05:46 +0600

    added gradescore.js

---
+++ conditionals/gradescore.js
@@ -0,0 +1,14 @@
+
+const score = 59;
+
+if(score >= 90 && score <=100){
+    console.log("you got A")
+}else if(score >= 80 && score <90){
+    console.log('you got B')
+}else if(score >= 70 && score <80){
+    console.log('you got C')
+}else if(score >= 60 && score <70){
+    console.log('you got D')
+}else {
+    console.log('you are fail')
+}
