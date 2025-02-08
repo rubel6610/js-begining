1a0a4b9cb97d35803b8e823b8333b0808a43f356
Rubel <rubelhosen1310@gmail.com>
2025-02-09 00:53:02 +0600

    practice with js loop

---
+++ loop/break.js
@@ -0,0 +1,22 @@
+// let num = 1;
+// while(num<=200){
+//     if(num === 101){
+//         break;
+//     }
+//     console.log(num);
+//     num++
+// }
+
+// // Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+// let startNum = 1;
+// let sum=0;
+// while(true){
+//     sum+=startNum;
+//     if(sum>=100){
+//         break;
+       
+//     }
+//     startNum++;
+// }
+// console.log("total sum", sum);
+// console.log("the number that caused the break", startNum);
