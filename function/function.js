8b29e3ab6dd9ce650424d349c042645cf3712a49
Rubel <rubelhosen1310@gmail.com>
2025-02-13 16:04:09 +0600

    practice with some js function

--- function/function.js
+++ function/function.js
@@ -11,3 +11,65 @@ function calculate(numMobile, NumLaptop, numTablet){
 const numDevices = calculate(3, 5,2);
 
 console.log(numDevices);
+
+
+// even and odd number with function
+
+function evenodd(num){
+    if(num%2 === 0){
+       let even =  num / 2;
+       return even;
+        
+    }
+   else if(num % 2 !== 0){
+      let odd =  num * 2
+        return odd;
+    }
+}
+
+const result = evenodd(11)
+console.log(result);
+
+// make average number
+
+function avg(arr){
+  let totalList = 0;
+    for (let list of arr){
+        totalList += list;
+       
+    }
+    let average = totalList / arr.length;
+       return average;
+ 
+}
+
+let averageResult = avg([28, 32, 55])
+console.log(averageResult);
+
+
+// count binary string
+
+function count_zero(binaryString){
+     let count = 0;
+     for(let char of binaryString){
+        if(char === "0" || char === 0){
+            count++;
+        }
+     }
+     return count;
+}
+let binaryStr = '101010101010';
+console.log(count_zero(binaryStr));
+
+// get even and odd num
+
+function odd_even(num){
+    if(num %2 === 0){
+        console.log("Number is even");
+       
+    }
+    else{
+        console.log("Number is odd");
+    }
+}
+odd_even(7);
