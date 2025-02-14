d609c339b632576ddd2b7af75c7d8f039de255ef
Rubel <rubelhosen1310@gmail.com>
2025-02-14 11:56:09 +0600

    count repeat number another way

--- function/countRepeatNum.js
+++ function/countRepeatNum.js
@@ -12,3 +12,21 @@ function countrepeat(num){
 }
 let result = countrepeat(numbers)
 console.log(result);
+
+// try another way of searching repeating numbers and find 25
+let list = [5,6,11,12,98,25, 5];
+let findNum = 25;
+let repeatNum = 0;
+function find(num, target){
+    for(i=0; i<list.length; i++){
+        let item = list.i;
+        if(item === target){
+            repeatNum++;
+        }else{
+            repeatNum=0;
+        }
+    }
+    return repeatNum;
+}
+let result2 = find(list,findNum)
+console.log(result2);
