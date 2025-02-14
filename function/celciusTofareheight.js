4dbb8f090f640f73e56d6c5dcfc01b9b0ea81940
Rubel <rubelhosen1310@gmail.com>
2025-02-14 11:42:16 +0600

    practice some problem of function

---
+++ function/celciusTofareheight.js
@@ -0,0 +1,5 @@
+function celciusTofaren(celcius){
+    let result = 9/5 * celcius + 32;
+    return result;
+}
+console.log(celciusTofaren(37));
---
+++ function/countRepeatNum.js
@@ -0,0 +1,14 @@
+
+let numbers = [5,6,11,12,98, 5];
+
+function countrepeat(num){
+    let count = 0;
+    for(number of num){
+        if(number === 5){
+            count++;
+        }
+    }
+    return count;
+}
+let result = countrepeat(numbers)
+console.log(result);
